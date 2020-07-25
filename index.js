67181b2913126f5b1f1780702a107e78e3eba500
alexmontdor69 <Alexandre LANDREAU>
2020-07-25 09:55:22 +0100

    Add get Result()

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// Expression object
+// Expression object - 
+//main expression => [2, "add", 3, "mutiply", epx-1]
 function Expression (id,upperLevel) {
     this.id = id;
     this.upperLevel=upperLevel;
@@ -7,6 +8,8 @@ function Expression (id,upperLevel) {
 } 
 
 // Operator Object
+// like add, sub, multiply... 
+// has there own mathematical definition (execute)
 function Ops (name,hasPriority,args,exeFunc) {
     this.name=name;
     this.hasPriority = hasPriority;
@@ -15,6 +18,7 @@ function Ops (name,hasPriority,args,exeFunc) {
 }
 
 // Calculus Object
+// Is the structure of one calcul
 function Calculus () {
     commands = [0];
     let currentExpression = 'main';
@@ -25,24 +29,24 @@ function Calculus () {
         currentExpression = expressionId;
     };
 
-this.createExpression = function (name, number, bracketState)
-{
-        if (bracketState=='bracketOpen'){
-            this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([name,'exp-'+Object.keys(this.expressionsList).length]);
-            this.expressionsList[Object.keys(this.expressionsList).length]=new Expression(Object.keys(this.expressionsList).length,this.expressionsList[currentExpression].id);
-            selectExpression (Object.keys(this.expressionsList).length-1);
-            this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([number]);
-        }
-        else if (bracketState=='bracketClose'){
-            this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([name,number]);
-            //bracket close so cursor go to parent expression
-            this.expressionsList[currentExpression].value=this.calculate (this.expressionsList[currentExpression].content[0],this.expressionsList[currentExpression].content)
-            selectExpression (this.expressionsList[currentExpression].upperLevel);
-        }
-        else 
-            this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([name,number]);
+    this.addToExpression = function (name, number, bracketState)
+    {
+            if (bracketState=='bracketOpen'){
+                this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([name,'exp-'+Object.keys(this.expressionsList).length]);
+                this.expressionsList[Object.keys(this.expressionsList).length]=new Expression(Object.keys(this.expressionsList).length,this.expressionsList[currentExpression].id);
+                selectExpression (Object.keys(this.expressionsList).length-1);
+                this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([number]);
+            }
+            else if (bracketState=='bracketClose'){
+                this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([name,number]);
+                //bracket close so cursor go to parent expression
+                this.expressionsList[currentExpression].value=this.calculate (this.expressionsList[currentExpression].content[0],this.expressionsList[currentExpression].content)
+                selectExpression (this.expressionsList[currentExpression].upperLevel);
+            }
+            else 
+                this.expressionsList[currentExpression].content=this.expressionsList[currentExpression].content.concat ([name,number]);
 
-    };
+        };
 
 
 
@@ -58,16 +62,19 @@ this.createExpression = function (name, number, bracketState)
     this.bracketClose = new Ops (')', true,'a,b', `return;`);
 
     this.calculate = function (LHS,RHS) {
-        console.log (RHS, RHS[0], typeof RHS[0])
+        //console.log (RHS,RHS.length, RHS[0], typeof RHS[0])
         
         if (typeof RHS[0] == "number") {
-            console.log('Number',RHS[0])
+            //console.log('Number',RHS[0])
             LHS=RHS[0]
 
-            if (RHS.length>1)
+            if (RHS.length>1){
+                //console.log("final result",this.calculate (LHS, RHS.slice(1)))
                 return this.calculate (LHS, RHS.slice(1))
+            }
         }
         if (typeof RHS[0] == "string"){
+
             if (RHS[0].slice(0,4)=="exp-")
                 return this.calculate(this.expressionsList[RHS[0].slice(4)].value,RHS.slice(1))
             //The operation has priority or not
@@ -75,6 +82,7 @@ this.createExpression = function (name, number, bracketState)
             // priority execute the next number
 
             if (this[RHS[0]].hasPriority){
+                //console.log ({LHS})
                 if (typeof RHS[1]== "number")
                     return this.calculate (this[RHS[0]].execute(LHS,RHS[1]), RHS.slice(2));
                 if (RHS[1].slice(0,4)=="exp-")
@@ -82,22 +90,26 @@ this.createExpression = function (name, number, bracketState)
                 
             }
             else {
-                console.log (
-                    LHS,
-                    this[RHS[0]],
-                    this.calculate (LHS, RHS.slice(1)),
-                    '=',
-                    this[RHS[0]].execute(LHS,this.calculate (LHS, RHS.slice(1))))
-                return this[RHS[0]].execute(LHS,this.calculate (LHS, RHS.slice(1)));
+                
+                //console.log ({LHS},this[RHS[0]].execute(LHS,this.calculate (LHS, RHS.slice(1))))
+
+                  return this[RHS[0]].execute(LHS,this.calculate (LHS, RHS.slice(1)));
         }
     }
-        return LHS
+        return LHS;
 
+    };
+    
+    
+    this.getResult = function (){
+        // save the value of the main expression
+        this.expressionsList['main'].value = this.calculate(this.expressionsList['main'].content[0],this.expressionsList['main'].content);
+        
+        return this.expressionsList['main'].value
     };
 
     Object.defineProperty(this,'commands', {
-        get: function() {return commands
-        }
+        get: function() {return commands}
     })
 }
 
@@ -105,13 +117,13 @@ this.createExpression = function (name, number, bracketState)
 const calcul = new Calculus();
 
 // 0+(2*(10+10)+3)*10/2-.5
-calcul.createExpression ('add',2,'bracketOpen');
-calcul.createExpression('multiply',10,'bracketOpen');
-calcul.createExpression ('add',10,'bracketClose');
-calcul.createExpression ('add',3,'bracketClose');
+calcul.addToExpression ('add',2,'bracketOpen');
+calcul.addToExpression('multiply',10,'bracketOpen');
+calcul.addToExpression ('add',10,'bracketClose');
+calcul.addToExpression ('add',3,'bracketClose');
 
-calcul.createExpression ('multiply',10);
-calcul.createExpression ('divide',2);
-calcul.createExpression ('sub',0.5);
+calcul.addToExpression ('multiply',10);
+calcul.addToExpression ('divide',2);
+calcul.addToExpression ('sub',0.5);
 
-calcul.calculate(calcul.expressionsList.main.content[0],calcul.expressionsList.main.content)
+calcul.getResult()
